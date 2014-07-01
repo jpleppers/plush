ba555d64861ddc7f89f5abd8fa570f57b776a3fa
Peter Leppers <p.leppers@nedforce.nl>
2014-07-01 17:06:41 +0200

    Possible fix for travis builds failing asset compilation

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -18,6 +18,11 @@ module Dummy
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Possible fix for asset compilation race conditions in Travis, see: https://github.com/jfirebaugh/konacha/issues/123 and https://github.com/sstephenson/sprockets/pull/441
+    config.assets.configure do |env|
+      env.cache = ThreadSafe::Cache.new
+    end
   end
 end
 
